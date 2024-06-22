2771994e24ba7210e0b41f8399530ae05c4da863
MinjeongKeum <135283706+Geumpall@users.noreply.github.com>
2024-06-22 16:39:49 +0900

    dd
    dd

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelector('#darkmode').addEventListener('click', function() {
-    document.getElementsByTagName('body')[0].classList.toggle('dark');
-    document.getElementsByTagName('h1')[0].classList.toggle('dark');
-    document.getElementsByTagName('H2')[0].classList.toggle('dark');
-    document.getElementsByTagName('ul')[0].classList.toggle('dark');
-    document.getElementsByTagName('p')[0].classList.toggle('dark');
+document.querySelector('#language').addEventListener('click', function() {
+    document.getElementsByTagName('body')[0].classList.toggle('kor');
+    document.getElementsByTagName('h1')[0].classList.toggle('kor');
+    document.getElementsByTagName('H2')[0].classList.toggle('kor');
+    document.getElementsByTagName('ul')[0].classList.toggle('kor');
+    document.getElementsByTagName('p')[0].classList.toggle('kor');
 });
